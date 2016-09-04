d7362acd8083a39d0ee80789e864e54357a78c86
zhangqing <735656869@qq.com>
2016-09-04 11:40:31 +0800

    develop 修改

--- src/cn/qing/zhang/demo/Demo1.java
+++ src/cn/qing/zhang/demo/Demo1.java
@@ -26,5 +26,6 @@ public class Demo1 {
         System.out.printf("输出一个浮点数：%f;输出一个十进制整数：%d;输出一个字符串:%s",d,a,s);
         System.out.println();
         System.out.printf("输出字符串%2$s,数字%1$d的十进制类型为%1$#x",a,s);//x$表示第几个参数
+        System.out.println("修改develop分支");
     }
 }
